9ff1d24e13c8f0dcf2524021dbcfe730e8d5658a
Jake Alsemgeest <alsemgeester@gmail.com>
2017-03-20 10:36:21 -0400

    The log has gravity now!

--- Assets/Scripts/PickUp.js
+++ Assets/Scripts/PickUp.js
@@ -1,16 +1,21 @@
 ﻿var target : Transform;
+public var rb : Rigidbody;
 
 function Update() {
 
 }
 
 function OnMouseDown() {
+	rb = GetComponent.<Rigidbody>();
+	rb.useGravity = false;
 	this.transform.position = target.position;
 	this.transform.parent = GameObject.Find("FPSController").transform;
 	this.transform.parent = GameObject.Find("FirstPersonCharacter").transform;
 }
 
 function OnMouseUp() {
+	rb.useGravity = true;
+	rb = null;
 	this.transform.parent = GameObject.Find("FPSController").transform;
 	this.transform.parent = null;
 }
